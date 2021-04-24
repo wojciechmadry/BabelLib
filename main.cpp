c66be5bb29def6f220de506e97467ed20602a5b6
mw46594 <mw46594@zut.edu.pl>
2021-04-24 15:17:04 +0200

    Decltype of storage type in atomic

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "babel.hpp"
 #include "tester.hpp"
 
+#include <atomic>
+
 int main()
 {
 
@@ -12,5 +14,10 @@ int main()
     TESTING::START_ALL_TEST(1);
     auto T = t.get_time();
     std::cout << "Done\nTime : " << T << " ms.\n";
+
+    std::atomic<int> d;
+    babel::CONCEPTS::type_in_atomic<decltype(d)>::type TT;
+    TT = 12;
+    std::cout << std::is_same_v<decltype(d), int> << '\n';
     return 0;
 }
