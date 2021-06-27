856a1b7af0173069ade54ab96732ed1d7d08696a
mw46594 <mw46594@zut.edu.pl>
2021-06-27 17:58:00 +0200

    little improvments

--- babLib/must_have.hpp
+++ babLib/must_have.hpp
@@ -7,7 +7,7 @@
 #include <variant>
 #include "concepts/concepts.hpp"
 #include <thread>
-
+#include "typdef.hpp"
 
 //FILE OPERATION
 #include <fstream>
@@ -34,55 +34,10 @@
     #include <Windows.h>
 #endif
 
-#ifndef i64
-#define i64 int64_t
-#endif
-
-#ifndef i32
-#define i32 int32_t
-#endif
-
-#ifndef i16
-#define i16 int16_t
-#endif
-
-#ifndef i8
-#define i8 int8_t
-#endif
-
-#ifndef u64
-#define u64 uint64_t
-#endif
-
-#ifndef u32
-#define u32 uint32_t
-#endif
-
-#ifndef u16
-#define u16 uint16_t
-#endif
-
-#ifndef u8
-#define u8 uint8_t
-#endif
-
-
-#ifndef float32
-#define float32 float
-#endif
-
-#ifndef float64
-#define float64 double
-#endif
-
-#ifndef float128
-#define float128 long double
-#endif
-
 
 namespace babel
 {
-    static constexpr const char* VERSION = "1.19";
+    static constexpr const char* VERSION = "1.20";
     static constexpr const bool COMPILER_IS_64B = (sizeof(void*) == 8); //NOLINT
     static constexpr const bool COMPILER_IS_32B = (sizeof(void*) == 4); //NOLINT
 }
